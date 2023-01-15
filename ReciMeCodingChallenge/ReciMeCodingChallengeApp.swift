7d70927053d2b1e20d278f36ed0208317fcea982
masterpaulo <dev.masterpaulo@gmail.com>
2023-01-15 15:24:04 +0800

    Initial commit

---
+++ ReciMeCodingChallenge/ReciMeCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReciMeCodingChallengeApp.swift
+//  ReciMeCodingChallenge
+//
+//  Created by John Paulo on 1/15/23.
+//
+
+import SwiftUI
+
+@main
+struct ReciMeCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
